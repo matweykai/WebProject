7ecc223a6ca7e5a78f996b08ae8e55ca6bdecd78
Matwey Ivanov <matveykai@list.ru>
2022-03-30 11:04:54 +0300

    Configure database
    Configured database and admin site for testing it

---
+++ Ynus/website/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    site_address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username + f'({self.site_address})'
+
+
+class Discipline(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Direction(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    img_path = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class DisciplineVote(models.Model):
+    direction = models.ForeignKey(Direction, on_delete=models.CASCADE)
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.direction}-{self.discipline}'
+
+
+class VoteCompany(models.Model):
+    vote = models.ForeignKey(DisciplineVote, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
