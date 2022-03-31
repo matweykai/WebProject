13dfa64d546b9b98ad8b215e1493e5f42345d02e
Matwey Ivanov <matveykai@list.ru>
2022-03-31 09:27:24 +0300

    Add directions page
    Changed names in css file, configured url and created template with
    template tag for indexing

--- Ynus/website/urls.py
+++ Ynus/website/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="main_page"),
     path('/about_univ', views.univ_info, name="about_univ"),
-    path('/about_project', views.project_info, name="project_info")
+    path('/about_project', views.project_info, name="project_info"),
+    path('/directions_view', views.directions_view, name="directions_view"),
 ]
--- Ynus/website/views.py
+++ Ynus/website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Direction
 
 
 def index(request):
@@ -11,3 +13,21 @@ def univ_info(request):
 
 def project_info(request):
     return render(request, 'website/about_project.html')
+
+
+def directions_view(request):
+    grdnt_lst = list()
+    directions = Direction.objects.all()
+
+    grnts = ['purple_grad', 'green_grad', 'blue_grad']
+    counter = 0
+
+    for i in range(len(directions)):
+        if i % 3 == 0:
+            counter += 1
+        grdnt_lst.append(grnts[(i + counter) % 3])
+
+    context = {'gradient_lst': grdnt_lst,
+               'directions': directions}
+
+    return render(request, 'website/directions.html', context=context)
