f79d80eabfd0ed510a6132f9bfb55e350f236129
Matwey Ivanov <matveykai@list.ru>
2022-04-24 11:59:29 +0300

    Change database scheme
    Added Vote model that represents company vote for the discipline.
    Removed proxy tables DisciplineVote and VoteCompany and replaced with
    corresponding relations in Discipline model

--- Ynus/website/models.py
+++ Ynus/website/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Company(models.Model):
+    """Represents company account in database"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(unique=True)
     site_address = models.CharField(max_length=50)
@@ -12,6 +13,7 @@ class Company(models.Model):
 
 
 class Discipline(models.Model):
+    """Represents direction disciplines (subjects) in database"""
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -19,25 +21,21 @@ class Discipline(models.Model):
 
 
 class Direction(models.Model):
+    """Represents direction (education programs) in database"""
     name = models.CharField(max_length=50)
     description = models.TextField()
     img_path = models.TextField()
+    disciplines = models.ManyToManyField(Discipline)
 
     def __str__(self):
         return self.name
 
 
-class DisciplineVote(models.Model):
-    direction = models.ForeignKey(Direction, on_delete=models.CASCADE)
-    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.direction}-{self.discipline}'
-
-
-class VoteCompany(models.Model):
-    vote = models.ForeignKey(DisciplineVote, on_delete=models.CASCADE)
+class Vote(models.Model):
+    """Represents company vote for the discipline in the database"""
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    direction = models.ForeignKey(Direction, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.vote}-{self.company}'
+        return f'{self.company.site_address}->{self.discipline.name}({self.direction.name})'
