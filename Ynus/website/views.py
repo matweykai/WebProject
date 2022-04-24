7750c465028a321c9ccb36a7f8d81ee715f8275f
Matwey Ivanov <matveykai@list.ru>
2022-04-24 12:25:02 +0300

    Change DirectionPage view and template
    Changed view and template, now they correctly work with current
    models

--- Ynus/website/views.py
+++ Ynus/website/views.py
@@ -4,26 +4,30 @@ from .models import Direction
 
 
 def index(request):
+    """Start page view"""
     return render(request, 'website/index.html')
 
 
 def univ_info(request):
+    """View for the page with university info"""
     return render(request, 'website/about_univ.html')
 
 
 def project_info(request):
+    """View for the page with project info"""
     return render(request, 'website/about_project.html')
 
 
 def directions_view(request):
-    grdnt_lst = list()
+    """View for the page with all directions that university has"""
+    grdnt_lst = list()  # stores colors of cells on the page
     directions = Direction.objects.all()
 
     grnts = ['purple_grad', 'green_grad', 'blue_grad']
     counter = 0
 
     for i in range(len(directions)):
-        if i % 3 == 0:
+        if i % len(grnts) == 0:
             counter += 1
         grdnt_lst.append(grnts[(i + counter) % 3])
 
@@ -34,6 +38,7 @@ def directions_view(request):
 
 
 class DirectionView(generic.DetailView):
+    """View for the page with direction info"""
     model = Direction
     template_name = "website/direction_page.html"
 
@@ -41,10 +46,9 @@ class DirectionView(generic.DetailView):
         context = super().get_context_data(**kwargs)
 
         all_voted_companies = list()
-
-        for discipline_vote in self.get_object().disciplinevote_set.all():
-            for vote_company in discipline_vote.votecompany_set.all():
-                all_voted_companies.append(vote_company.company.user.username)
+        # Counting all votes in this direction
+        for vote in self.get_object().vote_set.all():
+            all_voted_companies.append(vote.company.user.username)
 
         context['unique_votes_count'] = len(set(all_voted_companies))
         return context
